fdccab885541c31a12bea42adbd1be2534bb5061
Viktor Bespalov <bespalown@gmail.com>
2014-07-24 00:03:02 +0400

    Initial Commit

---
+++ Quins/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Quins
+//
+//  Created by Viktor Bespalov on 24/07/14.
+//  Copyright (c) 2014 bespalown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ QuinsTests/QuinsTests.m
@@ -0,0 +1,34 @@
+//
+//  QuinsTests.m
+//  QuinsTests
+//
+//  Created by Viktor Bespalov on 24/07/14.
+//  Copyright (c) 2014 bespalown. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QuinsTests : XCTestCase
+
+@end
+
+@implementation QuinsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
